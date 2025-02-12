fa40e0b2080dc9a8173ad40a009117cd92dbce3c
przemek83 <4788832+przemek83@users.noreply.github.com>
2025-02-12 14:30:54 +0100

    Clean main.cpp file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,13 @@
 #include <QApplication>
-#include "MainWindow.h"
 
-// #include <QTextCodec>
-#include <QTranslator>
+#include "MainWindow.h"
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
 
-    QTranslator translator;
-    translator.load("qt_pl.qm", QCoreApplication::applicationDirPath());
-    a.installTranslator(&translator);
-
-    //    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
-    //    QTextCodec::setCodecForTr (QTextCodec::codecForName ("UTF-8"));
-
     MainWindow w;
     w.show();
 
-    return a.exec();
+    return QApplication::exec();
 }
